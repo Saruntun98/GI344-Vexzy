55af0d8fe413dc32bc3ab6f1bc0e450a90c55803
Saruntun <saruntun.yimy@bumail.net>
2021-11-13 12:34:43 +0700

    Update settings

---
+++ Vexzy/Assets/Scripts/Manager/SettingsMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetVolume(float volume)
+    {
+        //Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
